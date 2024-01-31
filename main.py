d6661c85ca53004dc3ba171172761723ce49e4dc
IvanIzvekov <78640635+IvanIzvekov@users.noreply.github.com>
2024-01-31 17:53:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
+#1lab
+
 # def convert_to_grayscale(input_file, output_file):
 #     with open(input_file, 'rb') as f:
-#         # Считываем заголовок файла
 #         header = f.read(54)
 #
-#         # Считываем палитру
 #         palette = f.read(1024)
 #
-#         # Преобразуем палитру в оттенки серого
 #         new_palette = bytearray()
 #         for i in range(0, len(palette), 4):
 #             average_color = sum(palette[i:i+3]) // 3
 #             new_palette.extend([average_color, average_color, average_color, 0])
 #
-#         # Записываем новый файл
 #         with open(output_file, 'wb') as new_f:
 #             new_f.write(header)
 #             new_f.write(new_palette)
@@ -35,9 +33,9 @@
 #         print(f'File size: {file_size} bytes')
 #         print(f'Reserved 1: {reserved1}')
 #         print(f'Reserved 2: {reserved2}')
+#         print(f'Color depth: {color_depth}')
 #         print(f'Pixel array offset: {pixel_array_offset}')
 #
-# # Пример использования
 # input_file = 'CAT256.BMP'
 # output_file = 'CAT256_gray.BMP'
 #
@@ -45,56 +43,87 @@
 # convert_to_grayscale(input_file, output_file)
 
 
-import random
-import struct
 
-def add_random_border(input_file, output_file, border_width):
-    with open(input_file, 'rb') as f:
-        # Считываем заголовок файла
-        header = f.read(54)
+#2lab
 
-        # Считываем палитру (для TrueColor BMP палитры нет)
 
-        # Считываем данные изображения
-        image_data = f.read()
+# import random
+# import struct
+#
+# def add_random_border(input_file, output_file, border_width):
+#     with open(input_file, 'rb') as f:
+#         header = f.read(54)
+#
+#         image_data = f.read()
+#
+#         width = int.from_bytes(header[18:22], byteorder='little')
+#         height = int.from_bytes(header[22:26], byteorder='little')
+#         bits_per_pixel = int.from_bytes(header[28:30], byteorder='little')
+#
+#         border_colors = [bytes([random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]) for _ in range(border_width)]
+#
+#         new_image_data = bytearray()
+#         for y in range(height):
+#             for x in range(width):
+#                 if x < border_width or x >= width - border_width or y < border_width or y >= height - border_width:
+#                     new_image_data.extend(border_colors[x % border_width])
+#                 else:
+#                     new_image_data.extend(image_data[y * width * 3 + x * 3 : y * width * 3 + x * 3 + 3])
+#
+#             padding = b'\x00' * (4 - (len(new_image_data) % 4)) if len(new_image_data) % 4 != 0 else b''
+#             new_image_data.extend(padding)
+#
+#         new_header = bytearray(header)
+#         new_header[18:22] = width.to_bytes(4, byteorder='little')
+#         new_header[22:26] = height.to_bytes(4, byteorder='little')
+#
+#         with open(output_file, 'wb') as new_f:
+#             new_f.write(new_header)
+#             new_f.write(new_image_data)
+#
+# input_file = '_сarib_TC.bmp'
+# output_file = '_carib_TC_with_border.bmp'
+# # input_file = 'CAT256.BMP'
+# # output_file = 'CAT256.BMP_with_border.bmp'
+# border_width = 15
+#
+# add_random_border(input_file, output_file, border_width)
 
-        # Извлекаем информацию о размере изображения
-        width = int.from_bytes(header[18:22], byteorder='little')
-        height = int.from_bytes(header[22:26], byteorder='little')
-        bits_per_pixel = int.from_bytes(header[28:30], byteorder='little')
 
-        # Генерируем цвета для рамки
-        border_colors = [bytes([random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]) for _ in range(border_width)]
+#3lab
 
-        # Добавляем рамку к изображению
-        new_image_data = bytearray()
-        for y in range(height):
-            for x in range(width):
-                if x < border_width or x >= width - border_width or y < border_width or y >= height - border_width:
-                    new_image_data.extend(border_colors[x % border_width])
-                else:
-                    new_image_data.extend(image_data[y * width * 3 + x * 3 : y * width * 3 + x * 3 + 3])
+# def add_random_border(input_file, output_file):
+#     with open(input_file, 'rb') as f:
+#         header = f.read(54)
+#
+#         image_data = f.read()
+#
+#         width = int.from_bytes(header[18:22], byteorder='little')
+#         height = int.from_bytes(header[22:26], byteorder='little')
+#
+#         new_image_data = bytearray()
+#         for x in range(width):
+#             for y in range(height):
+#                 new_image_data.extend(image_data[y * width * 3 + x * 3 : y * width * 3 + x * 3 + 3])
+#
+#             padding = b'\x00' * (4 - (len(new_image_data) % 4)) if len(new_image_data) % 4 != 0 else b''
+#             new_image_data.extend(padding)
+#
+#         new_header = bytearray(header)
+#         new_header[18:22] = height.to_bytes(4, byteorder='little')
+#         new_header[22:26] = width.to_bytes(4, byteorder='little')
+#
+#         with open(output_file, 'wb') as new_f:
+#             new_f.write(new_header)
+#             new_f.write(new_image_data)
+#
+# input_file = '_сarib_TC.bmp'
+# output_file = '_carib_TC_rotate.bmp'
+#
+# add_random_border(input_file, output_file)
 
-            # Добавляем выравнивающие байты, если необходимо
-            padding = b'\x00' * (4 - (len(new_image_data) % 4)) if len(new_image_data) % 4 != 0 else b''
-            new_image_data.extend(padding)
 
-        # Обновляем заголовок файла с учетом изменений
-        new_header = bytearray(header)
-        new_header[18:22] = width.to_bytes(4, byteorder='little')
-        new_header[22:26] = height.to_bytes(4, byteorder='little')
 
-        with open(output_file, 'wb') as new_f:
-            new_f.write(new_header)
-            new_f.write(new_image_data)
 
-# Пример использования
-input_file = '_сarib_TC.bmp'
-output_file = '_carib_TC_with_border.bmp'
-# input_file = 'CAT256.BMP'
-# output_file = 'CAT256.BMP_with_border.bmp'
-border_width = 15
-
-add_random_border(input_file, output_file, border_width)
 
 
